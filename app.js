38b9b7979ad56393de54d6e803cf9514520bc856
timur kazdayev <tkazdayev@gmail.com>
2022-09-28 03:00:24 +0600

    add .env, edit app.js
    add send keys for text area and name commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
+const fs = require("fs");
 require('dotenv').config();
 
 let url = `https://github.com/retsaftu/sqat03`
@@ -12,7 +13,13 @@ let continueXpath = `//*[@id="login"]/div[4]/form/div/input[12]`
 let addFilesXpath = `//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[1]/div[2]/details/summary`
 let createNewFileXpath = `//*[@id="repo-content-pjax-container"]/div/div/div[3]/div[1]/div[2]/details/div/ul/li[3]/form/button`
 let newFileNameXpath = `//*[@id="repo-content-pjax-container"]/div/div/form[2]/div/div[1]/span/input`
+let fileContentXpath = `//*[@id="code-editor"]/div/pre/span`;
 
+let commitNameXpath = `//*[@id="commit-summary-input"]`
+let commitAdd = `add new file ${process.env.NEW_FILE_NAME} from ${process.env.FROM_FILE_NAME}`
+let commitAdd_M = `BTW this file ${process.env.NEW_FILE_NAME} add from this script ${process.env.FROM_FILE_NAME}`
+let commit_M = `//*[@id="commit-description-textarea"]`
+let commitNewFileXpath = `//*[@id="submit-file"]`
 let user = {
     email: process.env.USER_EMAIL,
     password: process.env.USER_PASSWORD
@@ -28,7 +35,8 @@ async function start() {
         .forBrowser('chrome')
         .setChromeOptions(options)
         .build()
-
+    const fromFile = await readFile();
+    console.log(`fromFile`, fromFile);
     try {
         await driver.get(url)
         await driver.sleep(3000)
@@ -49,6 +57,26 @@ async function start() {
         await driver.findElement(By.xpath(createNewFileXpath)).click()
         await driver.sleep(3000)
         await driver.findElement(By.xpath(newFileNameXpath)).sendKeys(process.env.NEW_FILE_NAME);
+        await driver.sleep(3000)
+        await driver.findElement(By.xpath(fileContentXpath)).click();
+        await driver.sleep(500)
+
+        await driver.findElement(By.xpath(fileContentXpath)).sendKeys(fromFile);
+        await driver.sleep(1000)
+
+        // await driver.findElement(By.xpath(fileContentXpath)).sendKeys(Key.CONTROL, 'a');
+        // await driver.sleep(500)
+
+        // await driver.findElement(By.xpath(fileContentXpath)).sendKeys(Key.CONTROL, 'c');
+        // await driver.sleep(500)
+
+        // await driver.findElement(By.xpath(fileContentXpath)).sendKeys(Key.CONTROL, 'v');
+        // await driver.sleep(500)
+        await driver.findElement(By.xpath(commitNameXpath)).sendKeys(commitAdd);
+        await driver.findElement(By.xpath(commit_M)).sendKeys(commitAdd_M);
+        await driver.sleep(5000)
+
+        await driver.findElement(By.xpath(commitNewFileXpath)).click();
 
 
         await driver.sleep(5000)
@@ -61,5 +89,11 @@ async function start() {
     }
 
 }
+async function readFile() {
 
+    let fileContent = await fs.readFileSync(process.env.FROM_FILE_NAME, "utf8");
+    // console.log(fileContent);
+    return fileContent
+}
 start()
+// readFile()
